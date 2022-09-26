4f7cefcf8f5ddc2f8b77ab72c141c65ca2dc4f2c
J365511 <J365511@TJESMAT02.TJESMAT>
2022-09-26 17:02:42 -0300

    First Commit

---
+++ src/application/Escalonamento.java
@@ -0,0 +1,94 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+import model.Processo;
+
+/**
+ * 
+ * @spetacularAuthor Juliana Chaves R. S. Paz
+ * 
+ */
+
+// FIFO
+public class Escalonamento {
+
+	static Scanner scan = new Scanner(System.in);
+	static List<Processo> arrayProcessos;
+	static Processo processo;
+	static Integer quantum;
+
+	public static void main(String[] args) {
+
+		char opcao = 's';
+		do {
+			
+			quantum = returnQuantum();
+			
+			System.out.println("+---------------------------------+");
+			System.out.println(" Quantidade de processos: [1 - 50] ");
+			int qtdProcessos = scan.nextInt();
+			if (qtdProcessos > 50 || qtdProcessos < 1) {
+				System.out.println("Opcao invalida!");
+				continue;
+			}
+
+			setProcesso(qtdProcessos);
+			sortArrivalTime();
+			printEscalonamento();
+			System.out.println("+---------------------------------+");
+
+			System.out.println("\nGerar outros processos? [S]im/[N]ao");
+			opcao = scan.next().toLowerCase().charAt(0);
+
+		} while (opcao == 's');
+
+	}
+
+	/**
+	 * Retorna o quantum de escalonamento
+	 */
+	public static int returnQuantum() {
+		return returnRandomValue(4, 12);
+	}	
+	
+	
+	/**
+	 * Instancia uma nova lista de processos e seta cada atributo dentro de cada
+	 * posicao na lista
+	 */
+	private static void setProcesso(int qtdProcessos) {
+		arrayProcessos = new ArrayList<>();
+		for (int i = 0; i < qtdProcessos; i++) {
+			processo = new Processo();
+			processo.setPid(Processo.returnPidTime(i + 1));
+			processo.setArrivalTime(Processo.returnArrivalTime(i));
+			processo.setBurstTime(Processo.returnBurstTime());
+			arrayProcessos.add(processo);
+		}
+	}
+
+	/**
+	 * Ordena a lista de processos de acordo com chegada dos processos (ArrivelTime)
+	 */
+	private static void sortArrivalTime() {
+		arrayProcessos.sort(Comparator.comparing(Processo::getArrivalTime));
+	}
+
+	/**
+	 * Imprimir cada posicao da lista de processos
+	 */
+	private static void printEscalonamento() {
+		System.out.println("Quantum: " + quantum);		
+		arrayProcessos.forEach(System.out::println);
+	}
+
+	public static int returnRandomValue(int min, int max) {
+		int random = (int) Math.floor(Math.random() * (max - min + 1) + min);
+		return random;
+	}
+	
+}
---
+++ src/model/Processo.java
@@ -0,0 +1,77 @@
+package model;
+
+import application.Escalonamento;
+
+public class Processo {
+
+	private String pid;
+	private Integer arrivelTime;
+	private Integer burstTime;
+
+	public Processo() {
+	}
+
+	public Processo(String pid, Integer arrivelTime, Integer burstTime) {
+		super();
+		this.pid = pid;
+		this.arrivelTime = arrivelTime;
+		this.burstTime = burstTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + pid + "  AT: " + arrivelTime + "  BT: " + burstTime;
+	}
+	
+	/**
+	 * Retorna o ID do processo 
+	 */
+	public static String returnPidTime(int idProcesso) {		
+		return "P" + idProcesso;
+	}
+
+	/**
+	 * Retorna o tempo de chegada de cada processo
+	 * Caso seja o primeiro processo, o retorno é 0
+	 */
+	public static Integer returnArrivalTime(int fistProcesso) {
+		if (fistProcesso == 0) {			
+			return 0;
+		}		
+		return Escalonamento.returnRandomValue(1, 30);
+	}
+
+	/**
+	 * Retorna o tempo estimado de execução de cada processo
+	 */
+	public static int returnBurstTime() {
+		return Escalonamento.returnRandomValue(1, 35);
+	}
+
+
+	
+	public String getPid() {
+		return pid;
+	}
+
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+
+	public Integer getArrivalTime() {
+		return arrivelTime;
+	}
+
+	public void setArrivalTime(Integer arrivelTime) {
+		this.arrivelTime = arrivelTime;
+	}
+
+	public Integer getBurstTime() {
+		return burstTime;
+	}
+
+	public void setBurstTime(Integer burstTime) {
+		this.burstTime = burstTime;
+	}
+
+}
